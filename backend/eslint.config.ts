3547530c2c8e5205adff5ed446278ee9f499006e
JustSxm <aypineb@gmail.com>
2025-06-09 23:10:27 -0400

    chore: ♻️ WIP - Edit eslint for backend

--- backend/eslint.config.ts
+++ backend/eslint.config.ts
@@ -1,5 +1,5 @@
 // @ts-check
-import eslint from '@eslint/js';
+import * as eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -20,7 +20,7 @@ export default tseslint.config(
       sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: __dirname,
       },
     },
   },
@@ -28,7 +28,8 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': 0,
     },
   },
 );
