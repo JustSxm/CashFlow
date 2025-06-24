d18338227be36e02bbd9425cd3fd626b3f63690f
JustSxm <aypineb@gmail.com>
2025-06-24 14:38:43 -0400

    feat: ✨ WIP TransactionView (Vue bug)

--- frontend/src/components/GreenInput.vue
+++ frontend/src/components/GreenInput.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 const props = defineProps<{
   type?: string
 }>()
 
 const model = defineModel()
 
-const type = props.type ?? 'text'
+let type = props.type ?? 'text'
+
+const isMoney = props.type === 'money'
+let min = undefined
+
+if (isMoney) {
+  type = 'number'
+  min = 0
+}
+
+function onInput(event: Event) {
+  let modifiedValue = 'a'
+  model.value = modifiedValue
+}
 </script>
 
 <template>
@@ -13,7 +28,7 @@ const type = props.type ?? 'text'
     <div class="absolute left-4 top-1/2 transform -translate-y-1/2">
       <slot></slot>
     </div>
-    <input :type="type" v-model="model" class="border border-1 border-green-500 text-black rounded-base w-full pl-10" />
+    <input :type="type" v-model="model" class="border border-green-500 text-black rounded-base w-full pl-10" :min="min" @input="onInput" />
   </div>
 </template>
 
