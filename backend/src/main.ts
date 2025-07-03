bec26fb2611af30a73b3b748dc12f2dcd4ff067b
JustSxm <aypineb@gmail.com>
2025-07-02 21:33:00 -0400

    feat: ✨ DRAFT: Production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://192.168.2.15:3000',
+    origin: ['http://192.168.2.15:3000', 'http://192.168.1.200:3000', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
