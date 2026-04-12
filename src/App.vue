838d2b1162c2a3a0ce52bee235e277bac6923a00
JustSxm <aypineb@gmail.com>
2026-04-11 20:54:39 -0400

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+</script>
+
 <template>
-  <div>
-    <h1>Welcome to your new project</h1>
-  </div>
+	<main class="px-3 pt-3 text-primary-950 flex flex-col gap-3">
+		<h1 class="font-bold text-xl">Home</h1>
+		<Card class="bg-linear-to-br from-primary-900 to-primary-700">
+			<CardHeader>
+				<CardTitle class="text-white">Card Title</CardTitle>
+				<CardDescription class="text-white">Card Description</CardDescription>
+			</CardHeader>
+			<CardContent class="text-white">
+				<p>Card Content</p>
+			</CardContent>
+		</Card>
+	</main>
 </template>
