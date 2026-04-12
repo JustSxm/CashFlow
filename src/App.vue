1fd3a3f017cdafe001378fefaa5c728765b883ae
JustSxm <aypineb@gmail.com>
2026-04-12 00:36:08 -0400

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowUpDoubleIcon, Chart03Icon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/vue";
+import { Badge } from "@/components/ui/badge";
 </script>
 
 <template>
-	<main class="px-3 pt-3 text-primary-950 flex flex-col gap-3">
-		<h1 class="font-bold text-xl">Home</h1>
-		<Card class="bg-linear-to-br from-primary-900 to-primary-700">
-			<CardHeader>
-				<CardTitle class="text-white">Card Title</CardTitle>
-				<CardDescription class="text-white">Card Description</CardDescription>
+	<main class="px-3 pt-3 flex flex-col gap-3 bg-background text-foreground">
+		<h1 class="text-lg">Welcome Back, Sam</h1>
+		<Card class="">
+			<CardHeader class="relative">
+				<CardTitle class="">Monthly Overview</CardTitle>
+				<div class="flex items-center gap-1 absolute right-4 top-0 flex-col">
+					<Badge class="bg-primary/20 text-primary"><HugeiconsIcon :icon="ArrowUpDoubleIcon" class="w-4 h-4 text-primary" />+15%</Badge>
+				</div>
 			</CardHeader>
-			<CardContent class="text-white">
-				<p>Card Content</p>
+			<CardContent class="flex flex-col gap-2">
+				<p class="text-4xl font-heading tabular-nums font-bold tracking-tight">934 $</p>
+
+				<span>
+					<span class="flex gap-1 items-center"><HugeiconsIcon :icon="ArrowUpDoubleIcon" class="w-4 h-4 text-primary" />Up 15% this month</span>
+					<span class="text-muted-foreground">versus the same date last month</span>
+				</span>
 			</CardContent>
 		</Card>
 	</main>
