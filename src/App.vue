e2e75d153226b86cb94682980e12b7a2631222c8
JustSxm <aypineb@gmail.com>
2026-04-12 21:14:35 -0400

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ArrowUpDoubleIcon, Chart03Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/vue";
 import { Badge } from "@/components/ui/badge";
+import Progress from "./components/ui/progress/Progress.vue";
 </script>
 
 <template>
@@ -24,6 +25,73 @@ import { Badge } from "@/components/ui/badge";
 					<span class="flex gap-1 items-center"><HugeiconsIcon :icon="ArrowUpDoubleIcon" class="w-4 h-4 text-primary" />Up 15% this month</span>
 					<span class="text-muted-foreground">versus the same date last month</span>
 				</span>
+			</CardContent>
+		</Card>
+		<div class="flex gap-2">
+			<Card class="w-full">
+				<CardHeader class="relative">
+					<CardTitle class="">Liquidity</CardTitle>
+					<div class="flex items-center gap-1 absolute right-4 top-0 flex-col">
+						<Badge class="bg-primary/20 text-primary"><HugeiconsIcon :icon="ArrowUpDoubleIcon" class="w-4 h-4 text-primary" />+15%</Badge>
+					</div>
+				</CardHeader>
+				<CardContent class="flex flex-col gap-2">
+					<div class="flex items-center">
+						<p class="text-4xl tabular-nums font-bold">11 934 $</p>
+					</div>
+
+					<span>
+						<span class="flex gap-1 items-center"><HugeiconsIcon :icon="ArrowUpDoubleIcon" class="w-4 h-4 text-primary" />Up 15% this month</span>
+						<span class="text-muted-foreground">versus last month</span>
+					</span>
+				</CardContent>
+			</Card>
+			<Card class="w-full">
+				<CardHeader class="relative">
+					<CardTitle class="">Surplus</CardTitle>
+					<div class="flex items-center gap-1 absolute right-4 top-0 flex-col">
+						<Badge class="bg-primary/20 text-primary"><HugeiconsIcon :icon="ArrowUpDoubleIcon" class="w-4 h-4 text-primary" />+15%</Badge>
+					</div>
+				</CardHeader>
+				<CardContent class="flex flex-col justify-between h-full">
+					<div class="flex items-center">
+						<p class="text-4xl tabular-nums font-bold">934 $</p>
+					</div>
+
+					<span>
+						<span class="flex gap-1 items-center"><HugeiconsIcon :icon="ArrowUpDoubleIcon" class="w-4 h-4 text-primary" />Up 15% last month</span>
+						<span class="text-muted-foreground">last month surplus</span>
+					</span>
+				</CardContent>
+			</Card>
+		</div>
+		<Card class="">
+			<CardHeader class="relative">
+				<CardTitle class="">Top Categories</CardTitle>
+				<div class="flex items-center gap-1 absolute right-4 top-0 flex-col text-xs text-muted-foreground">MONTH TO DATE</div>
+			</CardHeader>
+			<CardContent class="flex flex-col gap-4">
+				<div class="flex flex-col gap-2">
+					<div class="flex justify-between">
+						<span>Restaurants</span>
+						<span>820$</span>
+					</div>
+					<Progress :model-value="100" />
+				</div>
+				<div class="flex flex-col gap-2">
+					<div class="flex justify-between">
+						<span>Car</span>
+						<span>770$</span>
+					</div>
+					<Progress :model-value="88" />
+				</div>
+				<div class="flex flex-col gap-2">
+					<div class="flex justify-between">
+						<span>Groceries</span>
+						<span>720$</span>
+					</div>
+					<Progress :model-value="75" />
+				</div>
 			</CardContent>
 		</Card>
 	</main>
